950a87b51349feb8659730c319b8b66e6880e1fc
Luke Van Seters <lukevanseters@gmail.com>
2019-11-23 21:48:06 -0500

    Remove logo dim

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,7 +20,7 @@ export default ({ backgroundColor="white", children }) => (
     </Helmet>
     <div className={`flex bg-${backgroundColor}`}>
       <div className="pa4 w-100">
-        <div className="f5 lh-title mb3 fw9"><Link to="/" className="black link dim">vendorsheets.</Link></div>
+        <div className="f5 lh-title mb3 fw9"><Link to="/" className="black link">vendorsheets.</Link></div>
         {children}
       </div>
     </div>
